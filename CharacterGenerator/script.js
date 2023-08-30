43d4f9853ed8f50d7b104c0099e4eeb6617fe1e8
Jordan <hynoxdra@gmail.com>
2023-08-30 22:16:53 +1000

    appearance changes and code refactor

--- CharacterGenerator/script.js
+++ CharacterGenerator/script.js
@@ -1,48 +1,57 @@
-function getRandomElement(array) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-}
+// Define arrays to store the loaded data
+let races = [];
+let classes = [];
+let backgrounds = [];
+let firstNames = [];
+let lastNames = [];
 
-async function fetchData(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch data from ${url}`);
-    }
-    const textData = await response.text();
-    return textData.trim().split('\n');
-}
-
-async function generateCharacter() {
+// Function to load data from text documents
+async function loadData() {
     try {
-        const firstNameData = await fetchData('data/first-names.txt');
-        const lastNameData = await fetchData('data/last-names.txt');
-        const raceData = await fetchData('data/races.txt');
-        const charClassData = await fetchData('data/classes.txt');
-        const backgroundData = await fetchData('data/backgrounds.txt');
+        const raceResponse = await fetch('races.txt');
+        const classResponse = await fetch('classes.txt');
+        const backgroundResponse = await fetch('backgrounds.txt');
+        const firstNamesResponse = await fetch('firstnames.txt');
+        const lastNamesResponse = await fetch('lastnames.txt');
 
-        const firstName = getRandomElement(firstNameData);
-        const lastName = getRandomElement(lastNameData);
-        const race = getRandomElement(raceData);
-        const charClass = getRandomElement(charClassData);
-        const background = getRandomElement(backgroundData);
-
-        /*
-        const characterInfo = `
-        <h2>${firstName} ${lastName}</h2>
-        <p><strong>Race:</strong> ${race}</p>
-        <p><strong>Class:</strong> ${charClass}</p>
-        <p><strong>Background:</strong> ${background}</p>
-        `;
-        document.getElementById("characterInfo").innerHTML = characterInfo;
-        */
-
-        document.getElementById("characterName").textContent = firstName + " " + lastName;
-        document.getElementById("characterRace").textContent = race;
-        document.getElementById("characterClass").textContent = charClass;
-        document.getElementById("characterBackground").textContent = background;
+        races = (await raceResponse.text()).split('\n');
+        classes = (await classResponse.text()).split('\n');
+        backgrounds = (await backgroundResponse.text()).split('\n');
+        firstNames = (await firstNamesResponse.text()).split('\n');
+        lastNames = (await lastNamesResponse.text()).split('\n');
     } catch (error) {
-        console.error('Error:', error);
+        console.error("Error loading data:", error);
     }
 }
 
-generateCharacter();
+// Function to initialize the character generator
+async function initialize() {
+    await loadData();
+    generateCharacter(); // Generate character after loading data
+}
+
+// Function to generate a random character
+function generateCharacter() {
+    // Generate random indices
+    const randomRaceIndex = Math.floor(Math.random() * races.length);
+    const randomClassIndex = Math.floor(Math.random() * classes.length);
+    const randomBackgroundIndex = Math.floor(Math.random() * backgrounds.length);
+    const randomFirstNameIndex = Math.floor(Math.random() * firstNames.length);
+    const randomLastNameIndex = Math.floor(Math.random() * lastNames.length);
+
+    // Update character info
+    const nameElement = document.getElementById("characterName");
+    const raceElement = document.getElementById("characterRace");
+    const classElement = document.getElementById("characterClass");
+    const backgroundElement = document.getElementById("characterBackground");
+
+    const fullName = `${firstNames[randomFirstNameIndex]} ${lastNames[randomLastNameIndex]}`;
+
+    nameElement.textContent = fullName;
+    raceElement.textContent = races[randomRaceIndex];
+    classElement.textContent = classes[randomClassIndex];
+    backgroundElement.textContent = backgrounds[randomBackgroundIndex];
+}
+
+// Initialize once when the page loads
+initialize();
