17d99b709b322c1fa59d993d7ff3adfb7b4fbd02
Jordan H <Hynoxdra@gmail.com>
2023-09-21 22:07:26 +1000

    fix loading text documents

--- ManticoreGenerator/script.js
+++ ManticoreGenerator/script.js
@@ -1,3 +1,18 @@
+//Constants
+let heads = [];
+let torsos = [];
+let hinds = [];
+let tails = [];
+let wings =[];
+//mentalIllnesses = [];
+
+let SelectedHead = "";
+let SelectedBody = "";
+let SelectedHind = "";
+let SelectedTail = "";
+let SelectedWing = "";
+//let SelectedMentalIllness = "";
+
 function randomArrayValue(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
@@ -6,34 +21,40 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+async function LoadAsync() {
+    try {
+        const fetchUrls = [
+            'https://jordan-heath.github.io/ManticoreGenerator/data/heads.txt',
+            'https://jordan-heath.github.io/ManticoreGenerator/data/torsos.txt',
+            'https://jordan-heath.github.io/ManticoreGenerator/data/hinds.txt',
+            'https://jordan-heath.github.io/ManticoreGenerator/data/tails.txt',
+            'https://jordan-heath.github.io/ManticoreGenerator/data/wings.txt'
+        ];
+
+        const fetchResponses = await Promise.all(fetchUrls.map(url => fetch(url)));
+        const fetchTexts = await Promise.all(fetchResponses.map(response => response.text()));
+
+        [
+            heads,
+            torsos,
+            hinds,
+            tails,
+            wings
+        ] = fetchTexts.map(text => text.split('\n'));
+    } catch (error) {
+        console.error("Error loading data:", error);
+    }
+}
+
+const generateButton = document.getElementById("generateButton");
+const resultDisplay = document.getElementById("result");
+
 document.addEventListener("DOMContentLoaded", () => {
-    let SelectedHead = "";
-    let SelectedBody = "";
-    let SelectedHind = "";
-    let SelectedTail = "";
-    let SelectedWing = "";
-    //let SelectedMentalIllness = "";
-
-    const generateButton = document.getElementById("generateButton");
-    const resultDisplay = document.getElementById("result");
-
-    const headResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/heads.txt');
-    const torsoResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/torsos.txt');
-    const hindResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/hinds.txt');
-    const tailResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/tails.txt');
-    const wingResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/wings.txt');
-    //const mentalIllnessResponse = loadTextFile('https://jordan-heath.github.io/ManticoreGenerator/data/mentalIllnesses.txt');
-
-    heads = (headResponse.text()).split('\n');
-    torsos = (torsoResponse.text()).split('\n');
-    hinds = (hindResponse.text()).split('\n');
-    tails = (tailResponse.text()).split('\n');
-    wings = (wingResponse.text()).split('\n');
-    //mentalIllnesses = (mentalIllnessResponse.text()).split('\n');
+    LoadAsync();
 
     generateButton.addEventListener("click", () => {
         SelectedHead = randomArrayValue(heads);
-        SelectedBody = randomArrayValue(torso);
+        SelectedBody = randomArrayValue(torsos);
         SelectedHind = randomArrayValue(hinds);
         SelectedTail = randomArrayValue(tails);
         SelectedWing = randomArrayValue(wings);
