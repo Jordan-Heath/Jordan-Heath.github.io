5b61deed1b37b9bd6b154e014148fac7490253d4
Jordan H <jordanwork23@gmail.com>
2025-05-22 16:50:11 +1000

    cyberomniglot - typos and clarity

--- CyberOmniglot/js/data.js
+++ CyberOmniglot/js/data.js
@@ -19,12 +19,12 @@ const data = {
                             +"Those menues should allow you to talk to crewmates, access the dictionary, and configure the translator. ",
                 "The date": "The year is 2249 in the gregorian calendar. Specifically the date is October 2nd. "
                             +"You've been frozen in ice for at least 200 years. "
-                            +"Bet you didn't think that was possible, eh? We pumped you full of <a>Goop</a>. ",
+                            +"Bet you didn't think that was possible, huh? We pumped you full of <a>Goop</a>. ",
                 "Vocabulary": "You know like <a>Goop</a>, <a>Redvine</a>, <a>Octopastry</a>, or <a>Torpedont</a>. " 
                             + "You should be able to adjust your translator to understand these words. "
                             + "You'll want to open the Dictionary, give the words a meaningful name, and instruct the translator of the meaning of the words. "
                             + "I'm sure that sounds like a lot of nonsense, but you'll get there. ",
-                "Dictionary": "You had a dictionary in your time, right? This one works a bit differently as its focused on translating words. "
+                "Dictionary": "You had a dictionary in your time, right? This one works a bit differently as it's focused on translating words. "
                             + "Words you don't recognize will be placed in the dictionary so you can give them a new name and assign the meaning. "
                             + "Let me give you a suggestion: <a>Goop</a> could be renamed to 'Goop'. "
                             + "Then, when you see <a>Goop</a>, it will appear as <b>Goop</b> instead. "
@@ -57,15 +57,15 @@ const data = {
                             +"He can be a bit paranoid, but he means well.",
                 "Jemima Ethyl": "You should go meet Jemima Ethyl. When it comes to food, she will sort you out. "
                             +"I can't go a few hours, let alone 200 years without her lovely <a>Octopastry</a>.",
-                "Rook-7": "Rook-5 and I have a bit of a history. I was the one who took him apart. "
+                "Rook-5": "Rook-5 and I have a bit of a history. I was the one who took him apart. "
                             +"He was just soo anoying and Captain Vela-Ortiz wouldn't decommission him because he was getting results. ",
                 "Captain Vela-Ortiz": "I didn't think to mention her. "
                             +"She's the captain of the ship but between you and me, she doesn't do much more than throw her weight around. ",
                 "Eating": "Oh right, I had to install the translator in your mouth. I think I'll just have to hook you up with an intravenous <a>Goop</a> supply for your nutritional needs. "
                             +"Maybe we can get you a sleeker translator and a new synthetic tongue so you can start eating food again. ",
-                "Sight": "You're currently wired into a translator which presents you with a text interface. "
+                "Sight": "The translator is currently wired into your occipital lobe, affecting your visual perception. "
                             +"If it is working correctly, you should be able to see what I am saying on something like a screen. "
-                            +"This is a lot more interesting that relying on your regular sight, as your eyeballs were melted in the thawing process. "
+                            +"This is a lot more interesting than relying on your regular sight, given that your eyeballs were melted in the thawing process. "
                             +"Something with the <a>Goop</a> just does not agree with the eyes. ",
                 "Hearing": "Your inner ear was liquified in the thawing process meaning you can no longer naturally hear, but that shouldn't be a problem. "
                             +"The translator you are wired into also wires into your temporal lobe enabling you to 'hear' a synthesized voice. "
@@ -85,26 +85,26 @@ const data = {
                             +"But I take pity on you for how sad and weak you look. I'll give you a pass. ",
                 "<a>Naughty</a>": "Hey now, watch your language! I'll have to lock you in the hold for the rest of the voyage if you keep up that language with me. ",
                 "<a>Redvine</a>": "Did you find some? I swear to god if Jemima has brought some onto the ship again for a <a>Redvine</a> and <a>Octopastry</a> pie... "
-                            +"Anyway, report it to Rook-7. They can disable their smell sensors. ",
+                            +"Anyway, do me a favour and report it to Rook-7. Unlike me, they can disable their smell sensors. ",
                 "<a>Octopastry</a>": "I once went 200 days eating nothing but <a>Octopastry</a>. "
                             +"First it was awful, then I got used to it, now it's all that I want. "
                             +"Jemima Ethyl demands that I get a balanced diet - she's trying to feed me <a>Glubbos</a>. ",
-                "<a>Cramb</a>": "Vicious little creatures were going to tear their way through the hold of the ship. "
+                "<a>Cramb</a>": "Those vicious little creatures were going to claw their way through the hold of the ship. "
                             +"We would have been sucked into the vacuum of space and erupt from mouth to <a>Floppenge</a>. "
                             +"So I ordered Rook-7 to exterminate them all. ",
-                "<a>Glubbos</a>": "Don't tell Jermima Ethyl, but I have hidden the remaining store of <a>Glubbos</a> in the hold. "
+                "<a>Glubbos</a>": "Don't tell Jemima Ethyl, but I have hidden the remaining store of <a>Glubbos</a> in the hold. "
                             +"I'm going to get it tested for poison when we next reach port. ",
-                "<a>Torpedont</a>": "Beautiful munitions. I have a few in my quarters. " 
-                            +"I don't have much more room in my quarters, given the size of the <a>Torpedont</a>. "
-                            +"Anyway, due to the amount of explosive material, my quarters is officially classified as a ship magazine. ",
+                "<a>Torpedont</a>": "Beautiful munitions. 2 meters long, 0.5 meters wide and 0.5 meters thick - capable of making a crater 5 meters in diameter." 
+                            +"I keep one in my quarters if you want to see it in person. "
+                            +"Captain Vela-Ortiz had my quarters officially classified as a ship magazine just so I could keep a <a>Torpedont</a> in there. ",
                 "<a>Floppenge</a>": "Haha, I love that word. Go ask Professor Quill about it. ",
                 "The hold": "The hold is where we keep the food and supplies for the ship. We have cages for animals and stuff like that. ",
                 "Rook-7": "Rook-7 is the command droid of the ship. They're a good little guy. "
                             +"Ask them about the feeling of being crushed by a door. ",
-                "Jemima Ethyl": "She has a lot of potential, but she insists on making <a>Octopastry</a> and <a>Redvine</a> pies."
+                "Jemima Ethyl": "She has a lot of potential, but she insists on making <a>Octopastry</a> and <a>Redvine</a> pies. "
                             +"That and her insistence on using <a>Glubbos</a> in her cooking. I just want to eat <a>Octopastry</a>. ",
                 "Captain Vela-Ortiz": "She is a stupendous captain and a great leader. I aspire to be like her. ",
-                "Professor Quill": "She is a great medical officer. Usually if there is a security breach, its because of her. "
+                "Professor Quill": "She is a great medical officer. Usually if there is a security breach, it's because of her. "
                             +"As the medical officer she gets bored and does experiments which usually end up with someone getting hurt. "
                             +"Sorry to say but you're just another in a long line of experiments. ",
                 "The feeling of being crushed by a door": "I couldn't tell you what that feels like, but I bet Rook-7 could. "
@@ -120,23 +120,23 @@ const data = {
             topics: {
                 "Jemima Ethyl": "You have found the one and only! I make sure all the bellies are full so we can sail smoothly. "
                             +"I'm not sure what I can do for you given that big chunky translator obscuring your face. ",
-                "<a>Glubbos</a>": "Oh, well you see, I'm actually all out of <a>Glubbos</a>. It may not meet the same nutritional needs, but I do have <a>Octopastry</a>. ",
+                "<a>Glubbos</a>": "Well you see, I'm actually all out of <a>Glubbos</a>. It may not meet the same nutritional needs, but I do have <a>Octopastry</a>. ",
                 "<a>Octopastry</a>": "I have plenty of <a>Octopastry</a>! I will warn that relying entirely on <a>Octopastry</a> is not a good idea. "
                             +"I've heard it can make you a bit... loopy. ",
                 "<a>Naughty</a>": "Hunny, you know that's a naughty word. You should scrape your tongue out after saying that kind of thing. ",
                 "<a>Goop</a>": "Did you know that <a>Goop</a> actually contains all the nutrients you need to survive? "
                             +"Does me a fat load of good though. It has no taste, and that's a good thing. "
-                            +"The texture is horenndous - viscous like goop, but abrasive like sand. ",
+                            +"The texture is horrendous - viscous like goop, but abrasive like sand. ",
                 "<a>Redvine</a>": "I don't know what you're talking about. I don't have any <a>Redvine</a>. ",
                 "<a>Cramb</a>": "Deputy Slough exterminated all the <a>Cramb</a> on the ship. He said they had begun clawing through the cages. "
-                            +"I handle </a>Cramb</a> all the time, they're not dangerous. ",
-                "<a>Bopsi</a>": "Oh, <a>Bopsi</a> is a fizzy drink. I'm not sure if your stomach can handle it. I think I'm the only one on the ship who drinks it. ",
+                            +"I handle <a>Cramb</a> all the time, they're not dangerous. ",
+                "<a>Bopsi</a>": "<a>Bopsi</a> is a fizzy drink. I'm not sure if your stomach can handle it. I think I'm the only one on the ship who drinks it. ",
                 "Professor Quill": "She's a very smart lady. I don't really like going near her quarters though, she has a lot of weird stuff in there. "
                             +"If I knick myself cutting up a <a>Cramb</a> I'll say it's an emergency and call her to the kitchen. ",
                 "Deputy Slough": "He's a spoiled little brat. He thinks he can just eat <a>Octopastry</a> all day and not get sick. "
                             +"If only he would eat some <a>Glubbos</a> once in a while. ",
-                "Rook-3": "Oh, Rook-3 wasn't so bad. They were just a bit too enthusiastic about their work. "
-                            +"They shone like a beautiful star. Too bad that shining star closed my kitchen for 2 weeks. ",
+                "Rook-3": "Rook-3 wasn't so bad. They were just a bit too enthusiastic about their work. They spilled all the <a>Bopsi</a> on the floor and lost my temper. "
+                            +"After I struck them, they shone like a beautiful star. Too bad that shining star closed my kitchen for 2 weeks. ",
             }
         },
 
@@ -161,7 +161,7 @@ const data = {
                             +"One day they were hauling cargo from the hold. They were not paying attention around the door and it closed on them. "
                             +"Deputy Slough reported that it took 2 weeks to remove all the pieces of Rook-1 from the door. ",
                 "Rook-2": "Having learned from the mistakes of Rook-1, Rook-2 was designed to be more aware of their surroundings. "
-                            +"Having 3x the sensors of Rook-1, they were much more aware of their surroundings. "
+                            +"Having 3 times the sensors of Rook-1, they were much more aware of their surroundings. "
                             +"Unfortunately, they were also much more aware of the dangers of the ship. They became paranoid and refused to leave the hold. "
                             +"They lasted 1 week before they lost the patience of the crew. ",
                 "Rook-3": "Rook-3 was designed to be more confident. They were given a larger power supply and more powerful motors. "
@@ -189,7 +189,7 @@ const data = {
                 "Keelhauling": "Keelhauling is a punishment for a shipmate. The shipmate is tied to a rope and dragged under the ship. "
                             +"The shipmate is then pulled up on the other side of the ship. "
                             +"This is an obsurdly extraordinay punishment in the 23rd century. ",
-                "XB25628495": "XB25628495 used to be a emergency evacuation ship. After changes to company policy, emergecy evacuation ships were no longer required to be provided. "
+                "XB25628495": "XB25628495 used to be a emergency evacuation ship. After changes to company policy, emergency evacuation ships were no longer required to be provided. "
                             +"Now, XB25628495 is a cargo ship. I don't know how this small ship is supposed to carry enough cargo to be profitable. ",
                 "Changes to company policy": "There has been a lot of changes to company policy. Ask Captain Vela-Ortiz about it. ",
                 // "How are you?": "That is an interesting question. You might expect me to say 'I am a droid, I do not have feelings'. "
@@ -215,26 +215,29 @@ const data = {
                             +"Without me, only the AI steering the ship would be looking at the GPS. My eyes make sure that it's eyes are on the road. ",
                 "Captain": "I tell everyone what to do. If something comes up, I make the call. Just another of my laundry list of responsibilities. ",
                 "GPS": "Ah yes, I bet thats a new term for you. No technology like that existed in your time. "
-                            +"Its like a map and a compass, and a guide all in one. "
+                            +"It's like a map and a compass, and a guide all in one. "
                             +"It tells you where you are, where you're going, and how to get there. "
                             +"It makes my job a lot easier. ",
                 "The ship": "The ship is a small cargo ship. Her real name is XB25628495, but we call her 'The Ship'. It has a small crew of 5. "
                             +"We work for the Venture Agency for Galactic Exploration.  ",
-                "XB25628495": "Wow, I'm surprised you remembered that. It took me a whole month before I could remember it. ",
-                "Venture Agency for Galactic Exploration": "We use the whole term 'Venture Agency for Galactic Exploration' as the acronym is a bit rude. ",
+                "XB25628495": "Wow, I'm surprised you remembered that long sequence of meaningless numbers. It took me a whole month before I could remember it. ",
+                "Venture Agency for Galactic Exploration": "We're one of the great space companies of the 23rd century. "
+                            +"We use the whole term 'Venture Agency for Galactic Exploration' as the acronym is a bit rude. ",
                 "Medical Officer": "Professor Quill is the medical officer of the ship. She is experimenting with a defrosting agent to revive creatures from far before and far away. "
                             +"I don't mind, I think it's fun to have surprises in this job. ",
                 "Security Officer": "Deputy Slough is the security officer of the ship. He's my right hand man, and Rook-7 is his right hand... droid? ",
                 "Cook" : "Jemima Ethyl is the cook of the ship. She has an eclectic taste in food, which really spices things up - get it? ",
-                "Command Droid": "Rook-7 is the command droid. I got them for a steal. I reckon I've paid for the droid 3 times over in repairs. "
+                "Command Droid": "Rook-7 is the command droid. I got them for a steal. However, I reckon I've paid for the droid 3 times over in repairs. "
                             +"But they are a good little guy. ",
-                "Changes to company policy": "Oh, you know, the usual. "
+                "Changes to company policy": "Just the usual company shuffling, you know? "
                             +"We trimmed down on safety regulations because it's not like they work anyway. "
                             +"On the plus side, we aren't being babied by the company anymore! "
                             +"I get to choose my own crew, take whatever jobs I want, and go wherever I want. "
                             +"As long as I don't get caught. ",
                 "The crew": "I try and keep the crew slim and trim. We have a chef for food, medical officer for hurts, security officer for security, and a droid for... well, everything else. "
                             +"I don't need to be a jack of all trades, I just need to be the captain. ",
+                "Keelhauling": "You heard about that? Don't worry, I only did that to Rook-4 because I knew they would be fine."
+                            +"The vaccuum of space doesn't hurt a droid. ",
             }
         }
     },
